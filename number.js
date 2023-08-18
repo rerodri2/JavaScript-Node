831c9e21f1db5478160103d83bcfcd86a76c3a8d
Renan Rodrigues <renanrodrigues@MacBook-Air-de-Renan.local>
2023-08-17 22:00:26 -0300

    Exemplos da aula 1

---
+++ number.js
@@ -0,0 +1,26 @@
+const meuNumero = 3
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero / segundoNumero
+console.log(operacaoMatematica)
+
+//Números flutuantes
+
+const numeroPontoFlutuante = 3.3
+const numeroFlutuanteSemZero = .5
+const novaOperacao = numeroPontoFlutuante/numeroFlutuanteSemZero
+
+console.log(novaOperacao)
+
+const alura = "Alura"
+console.log(novaOperacao/alura)
+
+const meuNome = "Renan Da"
+const sobreNome = " Silva Rodrigues"
+console.log(meuNome + sobreNome)
+
+const verOPrimeiroNumero = 5
+const verOSegundoNumero = 5
+console.log(verOPrimeiroNumero === verOSegundoNumero)
